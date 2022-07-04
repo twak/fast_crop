3cf3d890a34624a36ade95a03d9885a549f6d43d
twak <twakelly@gmail.com>
2022-07-04 12:53:06 +0100

    crop and label per image

--- labels_json_to_img.py
+++ labels_json_to_img.py
@@ -13,20 +13,22 @@ import PIL.ImageDraw as ImageDraw
 import PIL.Image as Image
 from PIL import ImageOps
 
+colors = {}
+colors["window pane"] = (135, 170, 222)
+colors["window frame"] = (255, 128, 128)
+colors["open-window"] = (0, 0, 0)
+colors["wall frame"] = (233, 175, 198)
+colors["wall"] = (204, 204, 204)
+colors["door"] = (164, 120, 192)
+colors["shutter"] = (255, 153, 85)
+colors["blind"] = (255, 230, 128)
+colors["bars"] = (110, 110, 110)
+colors["balcony"] = (222, 170, 135)
+colors["misc object"] = (174, 233, 174)
+
 def render(dataset_root, photo_file, json_file):
 
-    colors = {}
-    colors["window pane"] = (135, 170, 222)
-    colors["window frame"] = (255, 128, 128)
-    colors["open-window"] = (0,0,0)
-    colors["wall frame"] = (233, 175, 198)
-    colors["wall"] = (204,204,204)
-    colors["door"] = (164, 120, 192)
-    colors["shutter"] = (255, 153, 85)
-    colors["blind"] = (255, 230, 128)
-    colors["bars"] = (110, 110, 110)
-    colors["balcony"] = (222,170,135)
-    colors["misc object"] = (174,233,174)
+    global colors
 
     # read src input
     photo = Image.open(os.path.join (dataset_root, "photos", photo_file) )
@@ -84,10 +86,56 @@ def render(dataset_root, photo_file, json_file):
             os.remove(to_del)
 
 
-def build_src_lookup(dataset_root):
+def render_per_crop(dataset_root, photo_file, json_file):
+    global colors
+
+    # read src input
+    photo = Image.open(os.path.join(dataset_root, photo_file))
+    # photo = ImageOps.exif_transpose(photo)
+
+    draw_label_trans = ImageDraw.Draw(photo, 'RGBA')
+
+    with open(json_file, "r") as f:
+        data = json.load(f)
+
+    label_photo = Image.new("RGB", (photo.width, photo.height))
+    draw_label_photo = ImageDraw.Draw(label_photo, 'RGBA')
+    draw_label_photo.rectangle([(0, 0), (label_photo.width, label_photo.height)], fill=(255, 255, 255))
+
+    # crop to each defined region
+    for crop_name, crop_data in data.items():
+
+        crop_bounds = crop_data["crop"]
+
+        # label_crop = Image.new("RGB", (crop_bounds[2] - crop_bounds[0], crop_bounds[3] - crop_bounds[1]))
+        # draw = ImageDraw.Draw(label_crop)
+
+        for cat, polies in crop_data["labels"].items():
+            # if cat != "wall frame":
+            #     continue
+            for poly in polies:
+                poly = [tuple(x) for x in poly]
+
+                # draw.polygon(poly, colors[cat] ) #random.randrange(0,255))
+
+                # poly = [(crop_bounds[0] + a, crop_bounds[1] + b) for (a, b) in poly]
+
+                # for idx, pt in poly:
+                #     poly[idx] = (crop_bounds[0] + pt[0], crop_bounds[1] + pt[1])
+
+                draw_label_photo.polygon(poly, colors[cat])
+                draw_label_trans.polygon(poly, (*colors[cat], 180), outline=(0, 0, 0))
+
+    jp = Path(json_file)
+    label_photo.save(os.path.join(jp.parent.parent, os.path.splitext(photo_file)[0] + ".png"), "PNG")
+    photo.save(os.path.join(jp.parent.parent, os.path.splitext(photo_file)[0] + ".jpg"), "JPEG")
+
+
+
+def build_src_lookup(lookup_file):
     src_lookup = {}
 
-    with open(r"C:\Users\twak\Documents\architecture_net\dataset\metadata_window_labels\from_labellers\input_locations_first_1500.txt", "r") as index_f:
+    with open(lookup_file, "r") as index_f:
         lines = index_f.readlines()
         for i in range(int(len(lines) / 2)):
             img_line = lines[i * 2]
@@ -101,9 +149,7 @@ def build_src_lookup(dataset_root):
     return src_lookup
 
 
-def write_label_jsons(dataset_root, src_lookup, lyd_json):
-
-
+def write_label_jsons( src_lookup, lyd_json, dataset_root = None):
 
     # we might have multiple labels from a single image
     # our data-structure assumes information-per-original-photo!
@@ -128,6 +174,10 @@ def write_label_jsons(dataset_root, src_lookup, lyd_json):
         for img_id, idx_info in idx_to_img.items():
 
             crop_file_name = idx_info["file_name"]
+
+            # if crop_file_name != "ee375aa4b1099b7a0649d1fe372138b5.jpg":
+            #     continue
+
             src_info = src_lookup[crop_file_name]
             src_file = src_info['src']
             src_region = src_info['crop_region']
@@ -147,48 +197,51 @@ def write_label_jsons(dataset_root, src_lookup, lyd_json):
             annotations_out[crop_file_name] = this_window
             this_window["crop"] = src_region
 
-            for ann in idx_to_ann.get(img_id):
-                cat_name = cdx_to_category[ann["category_id"]]
+            if img_id in idx_to_ann:
+                for ann in idx_to_ann.get(img_id):
+                    cat_name = cdx_to_category[ann["category_id"]]
 
-                polygon = ann["segmentation"]
-                polies_out = []
+                    polygon = ann["segmentation"]
+                    polies_out = []
 
-                for poly2 in polygon:
-                    poly_out = []
-                    for i in range ( int ( len ( poly2) /2 ) ):
-                        poly_out.append( ( poly2[i*2], poly2[i*2+1] ) )
-                    if len (poly_out) > 0:
-                        polies_out.append (poly_out)
+                    for poly2 in polygon:
+                        poly_out = []
+                        for i in range ( int ( len ( poly2) /2 ) ):
+                            poly_out.append( ( poly2[i*2], poly2[i*2+1] ) )
+                        if len (poly_out) > 0:
+                            polies_out.append (poly_out)
 
-                if cat_name in this_window['labels']:
-                    polygon_list = this_window['labels'][cat_name]
-                else:
-                    polygon_list = []
-                    this_window['labels'][cat_name] = polygon_list
+                    if cat_name in this_window['labels']:
+                        polygon_list = this_window['labels'][cat_name]
+                    else:
+                        polygon_list = []
+                        this_window['labels'][cat_name] = polygon_list
 
-                polygon_list.extend(polies_out)
+                    polygon_list.extend(polies_out)
 
-            print ("***********\n")
-            print (out_file_name)
-            # print (annotations_out)
+                print ("***********\n")
+                print (out_file_name)
+                # print (annotations_out)
 
-            with open(out_file_name, "w") as f:
-                json.dump(annotations_out, f)
+                with open(out_file_name, "w") as f:
+                    json.dump(annotations_out, f)
 
-            render (dataset_root, src_file, out_file_name)
+                # render (dataset_root, src_file, out_file_name)
+                render_per_crop (dataset_root, crop_file_name, out_file_name)
 
-            global COUNT
-            COUNT += 1
+                global COUNT
+                COUNT += 1
 
-dataset_root = r"C:\Users\twak\Documents\architecture_net\dataset"
+dataset_root = "/home/twak/Downloads/windowz_1500_1/" # r"C:\Users\twak\Documents\architecture_net\dataset"
 
 json_src = []
-json_src.extend(glob.glob(r'C:\Users\twak\Documents\architecture_net\dataset\metadata_window_labels\from_labellers\LYD__KAUST_1_batch_(100_frames)_21.06.2022\**.json'))
-src_lookup = build_src_lookup(dataset_root)
+json_src.extend(glob.glob(r'/home/twak/Downloads/LYD__KAUST_batch_2_24.06.2022/LYD<>KAUST_batch_2_24.06.2022/**.json'))
+src_lookup = build_src_lookup(r"/home/twak/Downloads/input_locations_first_1500.txt")
 
 COUNT = 0
 
 for f in json_src:
-    write_label_jsons(dataset_root, src_lookup, f)
+#    if f.endswith("8.json"):
+    write_label_jsons( src_lookup, f, dataset_root=dataset_root)
 
-print (f"processed {COUNT} windows")
+print ( f"processed {COUNT} windows" )
