4b9fa96d65e21ab299e43c659fe18acec7ce878d
twak <twakered@gmail.com>
2022-10-18 20:40:09 +0300

    untested, but running

--- tmp_split_dataset.py
+++ tmp_split_dataset.py
@@ -14,11 +14,13 @@ def split(rgb_files, output_folder, label_names=[["labels", "png"]], output_spli
     total = 0
     for p, n in output_split:
         total += p
-        cum_perc.append([ int ( total * len (rgb_files) / 100 ) , p, n])
+        cum_perc.append([ round ( total * len (rgb_files) / 100. ) , p, n])
 
     if total != 100:
         print("error, percents don't add up!")
         return
+
+    cum_perc[len (cum_perc)-1][0] += 10 # avoid rounding error on final cateogory
 
     cpi = 0
     for i, rgb in enumerate ( rgb_files ):
@@ -42,9 +44,36 @@ def split(rgb_files, output_folder, label_names=[["labels", "png"]], output_spli
         while i > cum_perc[cpi][0]: # cumulative > i
             cpi += 1
 
+
+#rgbs.extend(glob.glob(os.path.join( r"/ibex/scratch/kellyt/winlab_1/rgb", "*.jpg")))
+#split ( rgbs, r"/ibex/scratch/kellyt/winlab_2",
+#        label_names=[["labels", "png"]],
+#       output_split=[[50, "val"], [50, "test"]] )
+
+#rgbs.extend(glob.glob(os.path.join( r"/ibex/scratch/kellyt/winlab_1/england/rgb", "*.jpg")))
+#split ( rgbs, r"/ibex/scratch/kellyt/winlab_england_50_50",
+#        label_names=[["labels", "png"]],
+#        output_split=[[50, "val"], [50, "test"]] )
+ 
 rgbs = []
 
-rgbs.extend(glob.glob(os.path.join( r"/ibex/scratch/kellyt/winlab_1/rgb", "*.jpg")))
-split ( rgbs, r"/ibex/scratch/kellyt/winlab_2",
+rgbs.extend(glob.glob(os.path.join( r"/ibex/scratch/kellyt/winlab_1/austria/rgb", "*.jpg")))
+split ( rgbs, r"/ibex/scratch/kellyt/winlab_austria_50_50",
         label_names=[["labels", "png"]],
         output_split=[[50, "val"], [50, "test"]] )
+
+rgbs = []
+
+
+rgbs.extend(glob.glob(os.path.join( r"/ibex/scratch/kellyt/winlab_1/austria/rgb", "*.jpg")))
+split ( rgbs, r"/ibex/scratch/kellyt/winlab_austria_80_10_10",
+        label_names=[["labels", "png"]],
+        output_split=[[80, "train"],[50, "val"], [50, "test"]] )
+
+rgbs = []
+
+rgbs.extend(glob.glob(os.path.join( r"/ibex/scratch/kellyt/winlab_1/england/rgb", "*.jpg")))
+split ( rgbs, r"/ibex/scratch/kellyt/winlab_england_80_10_10",
+        label_names=[["labels", "png"]],
+        output_split=[[80, "train"],[50, "val"], [50, "test"]] )
+ 
