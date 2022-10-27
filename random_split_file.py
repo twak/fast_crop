8590c5245582f4adf771c11027b26b420641efe4
twak <twakered@gmail.com>
2022-10-27 21:44:35 +0100

    create split files

---
+++ random_split_file.py
@@ -0,0 +1,22 @@
+import glob
+import os
+import random
+import sys
+from pathlib import Path
+
+def random_split(location, count):
+
+    label_src = []
+    label_src.extend(glob.glob(os.path.join(location, "*.png")))
+    if len (label_src) < count:
+        print("not enough labels!")
+
+    random.shuffle(label_src)
+
+    with open("./split_%d.txt"%count, "w") as split:
+        for label in label_src[:count]:
+            print (label)
+            split.write( os.path.splitext ( Path(label).name)[0] + "\n" )
+
+if __name__ == "__main__":
+    random_split ( sys.argv[1], int ( sys.argv[2] ) )
