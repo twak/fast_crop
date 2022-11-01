4a5eab9bc88675fd541597b8ed8f171729015439
twak <twakered@gmail.com>
2022-11-01 18:10:15 +0000

    stronger check (that images can be opened)

--- tmp_valid_syn_pairs.py
+++ tmp_valid_syn_pairs.py
@@ -6,6 +6,7 @@ import contextlib
 import concurrent.futures
 
 # if blender rendering is interupted, we may not have all data for each image. Delete those which aren't complete...
+from PIL import Image
 
 def valid_syn_pairs(rgb_file):
 
@@ -16,11 +17,20 @@ def valid_syn_pairs(rgb_file):
 
     print (path)
     good = lambda f: os.path.exists(f) and os.path.getsize(f) > 0
-    if good(rgb_file) and good (lab_file) and good(attribs_file) and good (lab8_file):
+
+    bad = False
+    try:
+        a = Image.open(rgb_file, "r")    # albedo dataset causing issues
+        b = Image.open(lab8_file, "r")
+        if a is None or b is None:
+            bad = True
+    except:
+        bad = True
+
+    if good(rgb_file) and good (lab_file) and good(attribs_file) and good (lab8_file) and not bad:
         return 0 # good
     else:
         with contextlib.suppress(FileNotFoundError):
-
 
             if not good(rgb_file): print("missing rgb")
             if not good(lab_file): print("missing lab")
