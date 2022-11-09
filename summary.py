89eff3989ffe852da7e912c925033a7268d302bc
twak <twakered@gmail.com>
2022-11-09 10:22:19 +0000

    ...

--- summary.py
+++ summary.py
@@ -96,7 +96,7 @@ def batch_summary(dataset_root, batch):
                 else:
                     stats["rect_crops_win"] += 1
 
-                crop_bounds = rects["crop"]
+                crop_bounds = r["crop"]
                 crop_photo = img.crop(crop_bounds)
                 crop_save_img(basename, count, batch, dataset_root, crop_photo)
 
