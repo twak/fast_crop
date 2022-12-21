495b09378f7d22ca1a22d7edf278b9910288bd7a
tom kelly <twak@users.noreply.github.com>
2022-12-21 13:01:30 +0000

    script to sample first batch

--- tmp_sample_images_for_labellers.py
+++ tmp_sample_images_for_labellers.py
@@ -1,132 +1,132 @@
-import glob
+from glob import glob
 import hashlib
 import json
 import os
 import random
 import time
+from collections import defaultdict
 from pathlib import Path
 import PIL
 from PIL import Image, ImageOps
 
+import platform
 
-# script 18th July 2022 to second set of 1500 crops from dataset to send the labellers
-
-def build_src_lookup(lookup_file):
-    src_lookup = {}
-
-    with open(lookup_file, "r") as index_f:
-        lines = index_f.readlines()
-        for i in range(int(len(lines) / 2)):
-            img_line = lines[i * 2]
-            crop_line = lines[i * 2 + 1].replace('"', '').strip()
-            splits = img_line.split("[")
-            src_file_name = splits[0]
-            crop_region = splits[1].replace("]", "").strip()
-            crop_region = [*map(lambda x: int(x.strip()), crop_region.split(","))]
-            src_lookup[crop_line] = dict(src=src_file_name, crop_region=crop_region)
-
-    return src_lookup
+# script 19th December 22 to select unlabelled window crops for labelling.
 
 
 from itertools import filterfalse
 
-seen = build_src_lookup("./metadata_window_labels/from_labellers/input_locations_first_1500.txt")
-
-def in_seen(file):
-    global seen
-    for s in seen.values():
-        batch_name = s['src'].split("\\")
-        if batch_name[0].strip() in file and batch_name[1].strip() in file:
-            print(f"rejecting image from last set {file}")
-            return False
-    return True
-
-batches = os.listdir("./photos")
-
-photos = []
-photos.extend(glob.glob(r'./photos/*/*.jpg'))
-photos.extend(glob.glob(r'./photos/*/*.JPG'))
-# photos.extend(glob.glob(r'./photos/*/*9511.JPG'))
-
-photos[:] = filter(in_seen, photos)
-
-
-# json_src.extend(glob.glob( os.path.join (dataset_root, "metadata_window_labels", "*", "*.json" ) ) )
-
-
-output_dir = f"./metadata_single_elements/to_labellers_cook{time.time()}"
-os.makedirs(output_dir,exist_ok=True)
+output_dir = f"./metadata_for_labellers/to_labellers_cook{time.time()}"
+os.makedirs(output_dir, exist_ok=True)
 
 log = open(os.path.join(output_dir, 'log.txt'), "w")
 
-COUNT = 0
+all_batches = os.listdir("./photos")
+# batches = [x for x in batches if "michaela" in x]
+# batches.extend(["tom_london_20220418", "tom_york_20220411", "angela_prilep_20221022"])
 
-while COUNT < 2500:
+SEEN = set(glob(os.path.join("./metadata_window_labels", "*", "*.json")))
 
-    photo = random.choice(photos)
+def not_seen(jpgs):
+    global SEEN
+    sss = defaultdict(lambda: set())
+    out = []
 
-    print(f"{COUNT} : {photo}")
+    for s in SEEN:
+        sss[Path(s).parent.name].add(Path(s).with_suffix("").name)
 
-    batch = Path(photo).parent.name
-    if ("vienna" in batch or "Vienna" in batch) and random.random() < 0.3: # downsample boring grey vienna
-        print ("skipping Wein")
-        continue
+    for j in jpgs:
+        batch = Path(j).parent.name
+        base = Path(s).with_suffix("").name
 
-    min_dim = 1024
+        if base not in sss[batch]:
+            out.append(j)
+        else:
+            print(f"ignoring already labelled {j}")
 
-    out_name, out_ext = os.path.splitext(os.path.basename(photo))
-    json_file = Path(photo).parent.parent.parent.joinpath("metadata_single_elements").joinpath(batch).joinpath(f"{out_name}.json")
+    return out
 
-    if os.path.exists(os.path.join (".",json_file ) ): # crop
+for limit, batches, country in [
+    (1500, [x for x in all_batches if "michaela_vienna" in x], "austria"),
+    (500 , [x for x in all_batches if "michaela_berlin" in x], "germany"),
+    (300 , [x for x in all_batches if "tom" in x], "uk")
+    ]:
 
-        im = ImageOps.exif_transpose(Image.open(photo))
+    photos = set()
+    for b in batches:
+        photos.update (glob(f'./photos/{b}/*.jpg'))
+        if platform == "linux":
+            photos.add(glob(f'./photos/{b}/*.JPG'))
 
-        prev = json.load(open(json_file, "r") )
-        rects = prev["rects"]
+    print( f"all photos {len(photos)}" )
+    photos = not_seen(photos)
+    print(f"after removing labelled {len(photos) }" )
 
-        tags = []
+    COUNT = 0
 
-        if "tags" in prev:
-            tags = prev["tags"]
+    while COUNT < limit and len(photos) > 0:
 
-        if 'deleted' in tags:
-            print("skipping deleted")
-            continue
+        photo = random.sample(photos, k= 1)[0]
+        photos.remove(photo)
 
-        r = random.choice(rects)
+        print(f"{COUNT} : {photo}")
 
-        if "window" not in r[1] and "glass_facade" not in r[1] and "shop" not in r[1] and "church" not in r[1] and "abnormal" not in r[1]:
-            print("skipping not-a-window " + " ".join(tags))
-            continue
+        batch = Path(photo).parent.name
 
-        r = r[0]
+        min_dim = 2048
 
-        if r[2] - r[0] < min_dim or r[3] - r[1] < min_dim:
-            print("skipping small rect")
-            continue
+        out_name, out_ext = os.path.splitext(os.path.basename(photo))
+        json_file = Path(photo).parent.parent.parent.joinpath("metadata_single_elements").joinpath(batch).joinpath(f"{out_name}.json")
 
-        log.write("%s\a%s%s [%d, %d, %d, %d]\n" % (batch, out_name, out_ext, r[0], r[1], r[2], r[3]) )
+        if os.path.exists(os.path.join (".",json_file ) ): # crop
 
-        im = im.crop( ( r[0], r[1], r[2], r[3] ) )
+            im = ImageOps.exif_transpose(Image.open(photo))
 
-        md5hash = hashlib.md5(im.tobytes())
+            prev = json.load(open(json_file, "r") )
+            rects = prev["rects"]
 
-        jpg_out_file = "%s.jpg" % md5hash.hexdigest()
+            tags = []
 
-        log.write("\"%s\"\n" % jpg_out_file)
+            if "tags" in prev:
+                tags = prev["tags"]
 
-        out_path = os.path.join(output_dir, jpg_out_file)
+            if 'deleted' in tags:
+                print("skipping deleted")
+                continue
 
-        if (os.path.exists(out_path)):
-            print("skipping; duplicate hash!")
-            continue
+            r = random.choice(rects)
 
+            if "window" not in r[1] and "glass_facade" not in r[1] and "shop" not in r[1] and "church" not in r[1] and "abnormal" not in r[1]:
+                print("skipping not-a-window " + " ".join(tags))
+                continue
 
-        im.save(out_path, format="JPEG", quality=90)
-        COUNT += 1
-        log.flush()
-    else:
-        print("skipping missing json file")
+            r = r[0]
+
+            if r[2] - r[0] < min_dim or r[3] - r[1] < min_dim:
+                print("skipping small rect")
+                continue
+
+            im = im.crop( ( r[0], r[1], r[2], r[3] ) )
+
+            md5hash = hashlib.md5(im.tobytes())
+
+            jpg_out_file = "%s.jpg" % md5hash.hexdigest()
+
+            log.write("%s\\%s%s [%d, %d, %d, %d]\n" % (batch, out_name, out_ext, r[0], r[1], r[2], r[3]))
+            log.write("%s\n" % jpg_out_file)
+
+            out_path = os.path.join(output_dir, country, jpg_out_file)
+            os.makedirs(Path(out_path).parent, exist_ok=True)
+
+            if os.path.exists(out_path):
+                print("skipping; duplicate hash!")
+                continue
+
+            im.save(out_path, format="JPEG", quality=80)
+            COUNT += 1
+            log.flush()
+        else:
+            print("skipping: missing json file")
 
 
 log.close()
