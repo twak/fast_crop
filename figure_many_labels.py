cf0f87e9b2176e6a3289274819db0973beda4379
tom kelly <twak@users.noreply.github.com>
2023-03-15 12:28:16 +0000

    ...

--- figure_many_labels.py
+++ figure_many_labels.py
@@ -34,24 +34,14 @@ def render_labels_per_crop( json_files, dataset_root, output_folder, folder_per_
     xpos = -1
     ypos = 0
 
-    max_x = 5
-    max_y = 10
+    colors = process_labels.colours_for_mode(process_labels.PRETTY)
 
     for json_file in json_files:
 
         print (f"rendering crops from {json_file} @ {res}:{mode}")
-
-
-
         batch_name = Path(json_file).parent.name
 
-        # if not "tom_" in batch_name and not "michaela_" in batch_name:
-        #     return
-        #
-        # if "archive" in batch_name or "copenhagen" in batch_name:
-        #     return
 
-        global colors
 
         photo_file = process_labels.find_photo_for_json(dataset_root, json_file )
 
@@ -60,83 +50,63 @@ def render_labels_per_crop( json_files, dataset_root, output_folder, folder_per_
         with open(json_file, "r") as f:
             data = json.load(f)
 
-        photo =  process_labels.open_and_rotate( os.path.join(dataset_root, photo_file), data )
 
-        label_mode =  process_labels.label_color_mode()
 
         # crop to each defined region
         for crop_name, crop_data in data.items():
 
-            xpos += 1
-            if xpos >= max_x:
-                xpos = 0
-                ypos += 1
+            print(f" {xpos} -- {ypos} ")
 
-            if ypos > max_y:
-                svg_out.save()
-                return
 
             crop_bounds = crop_data["crop"]
-            crop_photo =photo.crop (crop_bounds)
+            photo_l = process_labels.open_and_rotate(os.path.join(dataset_root, photo_file), data)
+            photo_l  = photo_l.crop (crop_bounds)
 
-            label_img = Image.new(label_mode, (crop_photo.width, crop_photo.height))
-            draw_label_photo = ImageDraw.Draw(label_img, label_mode)
-            draw_label_photo.rectangle([(0, 0), (label_img.width, label_img.height)], fill=colors["none"] )
+            row_h = 800
+            scale = 0.1
+
+            r = row_h / photo_l.height
+            photo = photo_l.resize((int(photo_l.width * r), int(row_h)), resample=Image.Resampling.BOX)
+            s_im_name = f"{batch_name}_{Path(photo_file).name}.jpg"
+            photo.save(os.path.join(output_folder, s_im_name), format="JPEG")
+
+            l_xpos = xpos + photo.width * scale
+            ls = scale * r
+
+            svg_out.add(svg_out.image(href=s_im_name, insert=(xpos, ypos), size=(photo.width * scale, photo.height * scale)))
 
             for cat, polies in crop_data["labels"].items():
 
                 for poly in polies:
-                    poly = [tuple(x) for x in poly]
-                    draw_label_photo.polygon( poly, colors[cat])
+                    poly = [tuple((x[0] * ls + l_xpos, x[1] * ls + ypos)) for x in poly]
+                    svg_out.add(svg_out.polygon(poly, fill=f'rgb({colors[cat][0]},{colors[cat][1]},{colors[cat][2]})'))
 
-            # crop down
-            crop_photo = process_labels.crop(crop_photo, res, mode, resample=Image.Resampling.LANCZOS, background_col="black")
-            label_img  = process_labels.crop(label_img , res, mode, resample=Image.Resampling.NEAREST, background_col="white")
+            svg_out.add(svg_out.rect(
+                (l_xpos, ypos),
+                (photo.width * scale, photo.height * scale),
+                fill='none', stroke='black', stroke_width=0.5
+            ) )
 
-            base_name = os.path.splitext(crop_name)[0]
+            break # just do first crop..
 
-            crop_photo.save(os.path.join(output_folder, "rgb"   , base_name + ".jpg"))
-            label_img .save(os.path.join(output_folder, "labels", base_name + ".png"))
+        pad = 5
+        xpos += photo.width * scale * 2 + pad
+        if xpos >= 800:
+            xpos = 0
+            ypos += row_h * scale + pad
 
-
-            if True: # svg
-
-                label_img = Image.new(label_mode, (crop_photo.width, crop_photo.height))
-                draw_label_photo = ImageDraw.Draw(label_img, label_mode)
-                draw_label_photo.rectangle([(0, 0), (label_img.width, label_img.height)], fill=colors["none"])
-                dwg = svgwrite.Drawing(os.path.join(output_folder, "svg", crop_name + ".svg"), profile='tiny')
-                dwg.add(dwg.line((0, 0), (10, 0), stroke=svgwrite.rgb(10, 10, 16, '%')))
-                dwg.add(dwg.text(crop_name, insert=(0, 0.2), fill='black'))
-
-                for cat, polies in crop_data["labels"].items():
-                # for catl in crop_data["labels"]:
-
-                    # cat = catl[0]
-
-                    for poly in polies:
-                        poly = [tuple(x) for x in poly]
-                        draw_label_photo.polygon(poly, colors[cat])
-                        dwg.add(dwg.polygon(poly, fill=f'rgb({colors[cat][0]},{colors[cat][1]},{colors[cat][2]})'))
-
-                dwg.save()
-
-            if np_data is not None:
-                np_data.append( np.asarray(crop_photo) )
-
-
-
-
-VALID_CROPS = {'square_crop', 'square_expand', 'none'}
-
+        if ypos > 1200:
+            svg_out.save()
+            return
 
 if __name__ == "__main__":
 
     if platform == "win32":
-        dataset_root = r"C:\Users\twak\Documents\architecture_net\dataset"
+        dataset_root = r"E:\dataset"
+        output_folder = r"C:\Users\twak\Downloads\ad_labels"
     else:
         dataset_root = r"/datawaha/cggroup/kellyt/archinet_backup/complete_2401/data"
-
-    output_folder = r"/datawaha/cggroup/kellyt/iccv_add_mat/photos" #f"./metadata_single_elements/dataset_cook{time.time()}
+        output_folder = r"/datawaha/cggroup/kellyt/iccv_add_mat/labels"
 
     os.makedirs(output_folder, exist_ok=True)
 
@@ -146,7 +116,3 @@ if __name__ == "__main__":
     np_data = None  # []
 
     render_labels_per_crop(json_src, dataset_root, output_folder, folder_per_batch=False, res=-1, mode='none', np_data=np_data)
-
-    if np_data is not None:
-        all_data = np.concatenate(tuple(np_data), 0)
-        print(f"mean [{np.mean(all_data, axis=(0,1))}] std [{np.std(all_data, axis=(0,1))}]")
