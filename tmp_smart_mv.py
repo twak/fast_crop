4f6d2f9f0472b538707596629dad1fe4c0e03a33
tom kelly <twak@users.noreply.github.com>
2023-03-20 17:29:09 +0000

    ...

--- tmp_smart_mv.py
+++ tmp_smart_mv.py
@@ -16,19 +16,18 @@ if sys.platform != "win32":
 
 for jpg in jpgs:
     name, ext = os.path.splitext(Path(jpg).name)
-    # name = x[0]
-    # ext = x[1]
-    raw_file = os.path.join ( Path(jpg).parent, name +"."+ raw)
 
     extra = 0
     d_path = os.path.join( dest, f"{name}{ext}" )
+    r_path = os.path.join(Path(jpg).parent, name + "." + raw)
     while os.path.exists ( d_path ):
         extra +=1
         d_path = os.path.join ( dest, f"{name}_{extra}{ext}" )
+    r_path = os.path.join(Path(jpg).parent, f"{name}_{extra}.{raw}")
         print (f"trying {Path(jpg).name} to {d_path} ")
 
     shutil.copyfile(jpg, d_path)
-    if os.path.exists (raw_file):
-        shutil.copyfile(raw_file, os.path.join ( dest, f"{name}_{extra}.{raw}" ))
+    if os.path.exists (r_path):
+        shutil.copyfile(r_path, os.path.join (dest, f"{name}_{extra}.{raw}"))
     else:
         print ( f"missing raw {name}.{raw}" )
