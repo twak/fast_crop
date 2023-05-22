cee83d8f57265e8795d565e25d458ac3dac97226
tom kelly <twak@users.noreply.github.com>
2023-05-22 11:01:07 +0100

    tmp script bump

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
-# fast ccomputerop tool
+# fast crop tool
 
-This is a custom tool to mark window rectangles in many photos quickly. You can also tag the rectangles with additional metadata. It may be extended in the future to mark other features than rectangles...
+Tom's collections of scrips for dataset generation. 
 
-## dependencies
+## crop_tool
 
-Python 3.10, `Pillow` (9.1.1) `numpy` (1.22.3) and `pygame` (2.1.2).
-
-## interactive mode
+This is a custom tool to mark window rectangles in many photos quickly. You can also tag the rectangles with additional metadata. 
 
 ```
 python crop_tool.py "C:\Downloads\dataset\twak_london_20220522"
@@ -17,27 +15,13 @@ If the window is too big you can edit a line (about 286) `self.screen = pygame.d
 The interface is designed for throughput rather than comfort. The data is written when you advance to the next image, so if you make a mistake, close the program without advancing and start again!
 
 keys:
-* * `F1` - show rectangle-class cheat sheet
+
+* `F1` - show rectangle-class cheat sheet
 * `left` (or `space`), `right` - advance through the images in the folder. if you do not draw a rectangle, it will mark the whole photo as a window. To not include any rectangles, press `0` to ignore the photo entirely.
 * mouse - draw rectangle. all new rectangles are currently given the `window` tag or the tags from the current rectangle.
 * pink letters at top left: add/remove tags to the current rectangle or entire image
 * `up`, `down` - change the current rectangle
 * `backspace` - remove current rectangle
 * `c` - remove all rectangles from current photo.
-* `0` - ignore/don't ignore this photo (any marked rectangles will be ignored)
+* `0` - toggle soft delete/ignore (any marked rectangles will be ignored)
 * `z` - cycle cursor modes (crosshairs/zoom) 
-
-
-
-## dataset generation mode
-
-To generate the dataset in folder `dataset1`:
-
-```
-python crop_tool.py "C:\Downloads\dataset\twak_london_20220522" "C:\Downloads\dataset_1" 
-```
-
-You can optionally set a crop mode ('square_crop', 'square_expand', 'none') and a resolution (pixels) as additional arguments. The following crops squares from inside the window-rectangles at a resolution of 512 pixels.
-```
-python crop_tool.py "C:\Downloads\dataset\twak_london_20220522" "C:\Downloads\dataset\dataset_1" square_crop 512
-```
--- tmp_build_crops_2.py
+++ tmp_build_crops_2.py
@@ -15,8 +15,11 @@ if __name__ == "__main__":
     else:
         dataset_root = r"/mnt/vision/data/archinet/data"
 
-    src_lookup = single_one_off_import_lyd_json.build_src_lookup([r"C:\Users\twak\Documents\architecture_net\windows_part3\log_part_3.txt",
-                                   r"C:\Users\twak\Documents\architecture_net\windows_part3\log_part_4.txt"])
+    src_lookup = single_one_off_import_lyd_json.build_src_lookup(
+        [r"C:\Users\twak\Documents\architecture_net\windows_part3\log_part_3.txt",
+         r"C:\Users\twak\Documents\architecture_net\windows_part3\log_part_4.txt",
+         r"C:\Users\twak\Documents\architecture_net\windows_part3\log_part_5.txt",
+         r"C:\Users\twak\Documents\architecture_net\windows_part3\log_part_6.txt"])
 
     images = []
     images.extend(glob.glob(r'C:\Users\twak\Documents\architecture_net\windows_part3\crops\**.jpg'))
@@ -26,20 +29,21 @@ if __name__ == "__main__":
         for i in images:
             src_info = src_lookup[Path(i).name]
             # print (src_info['src'])
-            out = os.path.join (dataset_root, "metadata_winodw_labels_2", Path ( src_info['src'] ).with_suffix(".json") )
+            out = os.path.join (dataset_root, "metadata_window_labels_2", Path ( src_info['src'] ).with_suffix(".json") )
             os.makedirs(Path(out).parent, exist_ok=True)
             with open(out, 'w') as _:
                 pass
 
-    if False:
-        # these are the files in our format
-        for i in glob.glob(r'C:\Users\twak\Documents\architecture_net\windows_part3\labels_all_labelled\*.json'):
+    if True:
+        # these are the files in our format, but need moving to correct batch folder
+        for i in glob.glob(r'C:\Users\twak\Documents\architecture_net\windows_part3\labels_all_labels_so_far\*.json'):
             src_info = src_lookup[Path(i).with_suffix(".jpg").name]
-            out = os.path.join(dataset_root, "metadata_labels_2", Path(src_info['src']).with_suffix(".json"))
+            out = os.path.join( dataset_root, "metadata_window_labels_2", Path(src_info['src']).with_suffix(".json") )
             print(src_info['src'])
+            os.makedirs(Path(out).parent, exist_ok=True)
             shutil.copyfile(i, out)
 
-    if True:
+    if False:
         # count labels for both sets:
 
         count = defaultdict(lambda: 0)
