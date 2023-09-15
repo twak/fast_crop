86c891c3f581e977029be2343df57b6bf7dc666d
twak <twakered@gmail.com>
2023-09-15 09:44:00 +0100

    ...

--- scripts/blender_post.sh
+++ scripts/blender_post.sh
@@ -4,7 +4,7 @@ python ~/fast_crop/blender_labels_to_dataset.py .
 python ~/fast_crop/blender_complete_renders.py . really
 # do histogram matching on rgb + exposed images
 python ~/fast_crop/match_histograms.py rgb /ibex/user/kellyt/winlab_1/rgb rgb_histomatched
-python ~/fast_crop/match_histograms.py exposed /ibex/user/kellyt/winlab_1/rgb exposed_histomatched
+#python ~/fast_crop/match_histograms.py exposed /ibex/user/kellyt/winlab_1/rgb exposed_histomatched
 
 # create split files
 cd rgb
