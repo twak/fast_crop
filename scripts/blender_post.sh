afa96a9e8457729867bf3d64bd10b41a386fcc4d
twak <twakered@gmail.com>
2023-09-16 09:45:07 +0100

    ...

--- scripts/blender_post.sh
+++ scripts/blender_post.sh
@@ -4,7 +4,7 @@ python ~/fast_crop/blender_labels_to_dataset.py .
 python ~/fast_crop/blender_complete_renders.py . really
 # do histogram matching on rgb + exposed images
 python ~/fast_crop/match_histograms.py rgb /ibex/user/kellyt/winlab_1/rgb rgb_histomatched
-#python ~/fast_crop/match_histograms.py exposed /ibex/user/kellyt/winlab_1/rgb exposed_histomatched
+python ~/fast_crop/match_histograms.py exposed /ibex/user/kellyt/winlab_1/rgb exposed_histomatched
 
 # create split files
 cd rgb
@@ -16,7 +16,7 @@ cat all_shuf.txt | head -n 1024 > 1024.txt
 # tail -n 2000 all_shuf.txt > pval.txt
 
 # build parameters for efficientNet
-python ~/efficient/blender_process_attribs.py .
+#python ~/efficient/blender_process_attribs.py .
 
 # create image-grids for professors
 python ~/fast_crop/tmp_names_to_grid.py . "1024.txt" "labels" "png"
@@ -24,4 +24,4 @@ python ~/fast_crop/tmp_names_to_grid.py . "1024.txt" "exposed" "png"
 python ~/fast_crop/tmp_names_to_grid.py . "1024.txt" "rgb" "png"
 
 # print std and mean for synthetic photos to terminal (you'll want to save these for training)
-python ~/fast_crop/find_mean_std.py "exposed" 1024
+python ~/fast_crop/find_mean_std.py "rgb" 1024
