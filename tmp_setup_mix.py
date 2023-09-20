23ce4fa61deca97bed687ae80529d9eb3c5074e9
twak <twakered@gmail.com>
2023-09-20 19:37:55 +0100

    ...

--- tmp_setup_mix.py
+++ tmp_setup_mix.py
@@ -5,42 +5,41 @@ from PIL import Image
 
 outdir = "/ibex/user/kellyt/mix"
 
-if False:
 
-    os.makedirs(f"{outdir}/rgb", exist_ok=True)
-    os.makedirs(f"{outdir}/labels", exist_ok=True)
+os.makedirs(f"{outdir}/rgb", exist_ok=True)
+os.makedirs(f"{outdir}/labels", exist_ok=True)
 
-    rlines, slines = [], []
+rlines, slines = [], []
 
-    with open("/ibex/user/kellyt/winlab_5/2048.txt", "r") as f:
-        rlines = f.readlines()
-        for line in rlines:
-            print(line)
-            line = line.strip()
-            shutil.copyfile("/ibex/user/kellyt/winlab_5/rgb/" + line + ".jpg", f"{outdir}/rgb/{line}.jpg")
-            shutil.copyfile("/ibex/user/kellyt/winlab_5/labels/" + line + ".png", f"{outdir}/labels/{line}.png")
+with open("/ibex/user/kellyt/winlab_5/2048.txt", "r") as f:
+    rlines = f.readlines()
+    # for line in rlines:
+    #     print(line)
+    #     line = line.strip()
+    #     shutil.copyfile("/ibex/user/kellyt/winlab_5/rgb/" + line + ".jpg", f"{outdir}/rgb/{line}.jpg")
+    #     shutil.copyfile("/ibex/user/kellyt/winlab_5/labels/" + line + ".png", f"{outdir}/labels/{line}.png")
 
 
-    with open("/ibex/user/kellyt/windowz/winsyn_riyal/2048.txt", "r") as f:
-        slines = f.readlines()
-        for line in slines:
-            print(line)
-            line = line.strip()
-            img = Image.open(f"/ibex/user/kellyt/windowz/winsyn_riyal/rgb/{line}.png")
-            img = img.convert("RGB")
-
-            # background = Image.new('RGBA', img.size, (255, 255, 255))
-            # alpha_composite = Image.alpha_composite(background, img)
-
-            img.save(f"{outdir}/rgb/{line}.jpg", "JPEG", quality=90)
-            shutil.copyfile("/ibex/user/kellyt/windowz/winsyn_riyal/labels/" + line + ".png", f"{outdir}/labels/{line}.png")
+with open("/ibex/user/kellyt/windowz/winsyn_riyal/2048.txt", "r") as f:
+    slines = f.readlines()
+    # for line in slines:
+    #     print(line)
+    #     line = line.strip()
+    #     img = Image.open(f"/ibex/user/kellyt/windowz/winsyn_riyal/rgb/{line}.png")
+    #     img = img.convert("RGB")
+    #
+    #     # background = Image.new('RGBA', img.size, (255, 255, 255))
+    #     # alpha_composite = Image.alpha_composite(background, img)
+    #
+    #     img.save(f"{outdir}/rgb/{line}.jpg", "JPEG", quality=90)
+    #     shutil.copyfile("/ibex/user/kellyt/windowz/winsyn_riyal/labels/" + line + ".png", f"{outdir}/labels/{line}.png")
 
 
 for i in range(5):
 
     real = 2 ** i
 
-    print(f">>>> {real}")
+    print(f">>>> {real} {i}")
 
     with open(f"{outdir}/mix_{real}.txt", "w") as f:
 
