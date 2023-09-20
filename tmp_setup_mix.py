54fc7badf5ffb262c7e57e941a7f45e13822ce36
twak <twakered@gmail.com>
2023-09-20 21:47:23 +0100

    ...

--- tmp_setup_mix.py
+++ tmp_setup_mix.py
@@ -5,6 +5,9 @@ from PIL import Image
 
 outdir = "/ibex/user/kellyt/mix"
 
+'''
+create a new dataset that mixes synthetic and real. Create a bunch of split files for different ratios. Trained on 0079_mix
+'''
 
 os.makedirs(f"{outdir}/rgb", exist_ok=True)
 os.makedirs(f"{outdir}/labels", exist_ok=True)
@@ -13,29 +16,29 @@ rlines, slines = [], []
 
 with open("/ibex/user/kellyt/winlab_5/2048.txt", "r") as f:
     rlines = f.readlines()
-    # for line in rlines:
-    #     print(line)
-    #     line = line.strip()
-    #     shutil.copyfile("/ibex/user/kellyt/winlab_5/rgb/" + line + ".jpg", f"{outdir}/rgb/{line}.jpg")
-    #     shutil.copyfile("/ibex/user/kellyt/winlab_5/labels/" + line + ".png", f"{outdir}/labels/{line}.png")
+    for line in rlines:
+        print(line)
+        line = line.strip()
+        shutil.copyfile("/ibex/user/kellyt/winlab_5/rgb/" + line + ".jpg", f"{outdir}/rgb/{line}.jpg")
+        shutil.copyfile("/ibex/user/kellyt/winlab_5/labels/" + line + ".png", f"{outdir}/labels/{line}.png")
 
 
 with open("/ibex/user/kellyt/windowz/winsyn_riyal/2048.txt", "r") as f:
     slines = f.readlines()
-    # for line in slines:
-    #     print(line)
-    #     line = line.strip()
-    #     img = Image.open(f"/ibex/user/kellyt/windowz/winsyn_riyal/rgb/{line}.png")
-    #     img = img.convert("RGB")
-    #
-    #     # background = Image.new('RGBA', img.size, (255, 255, 255))
-    #     # alpha_composite = Image.alpha_composite(background, img)
-    #
-    #     img.save(f"{outdir}/rgb/{line}.jpg", "JPEG", quality=90)
-    #     shutil.copyfile("/ibex/user/kellyt/windowz/winsyn_riyal/labels/" + line + ".png", f"{outdir}/labels/{line}.png")
+    for line in slines:
+        print(line)
+        line = line.strip()
+        img = Image.open(f"/ibex/user/kellyt/windowz/winsyn_riyal/rgb/{line}.png")
+        img = img.convert("RGB")
+
+        # background = Image.new('RGBA', img.size, (255, 255, 255))
+        # alpha_composite = Image.alpha_composite(background, img)
+
+        img.save(f"{outdir}/rgb/{line}.jpg", "JPEG", quality=90)
+        shutil.copyfile("/ibex/user/kellyt/windowz/winsyn_riyal/labels_8bit/" + line + ".png", f"{outdir}/labels/{line}.png")
 
 
-for i in range(10):
+for i in range(12):
 
     real = 2 ** i
 
