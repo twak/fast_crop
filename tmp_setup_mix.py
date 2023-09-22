003332ea8f436529d97890a1d9899e9033e605e7
twak <twakered@gmail.com>
2023-09-22 21:58:10 +0100

    ...

--- tmp_setup_mix.py
+++ tmp_setup_mix.py
@@ -23,39 +23,45 @@ with open("/ibex/user/kellyt/winlab_5/2048.txt", "r") as f:
     #     shutil.copyfile("/ibex/user/kellyt/winlab_5/labels/" + line + ".png", f"{outdir}/labels/{line}.png")
 
 
-with open("/ibex/user/kellyt/windowz/winsyn_riyal/2048.txt", "r") as f:
+with open("/ibex/user/kellyt/windowz/winsyn_riyal/16384.txt", "r") as f:
     slines = f.readlines()
-    # for line in slines:
-    #     print(line)
-    #     line = line.strip()
-    #     img = Image.open(f"/ibex/user/kellyt/windowz/winsyn_riyal/rgb/{line}.png")
-    #     img = img.convert("RGB")
-    #
-    #     # background = Image.new('RGBA', img.size, (255, 255, 255))
-    #     # alpha_composite = Image.alpha_composite(background, img)
-    #
-    #     img.save(f"{outdir}/rgb/{line}.jpg", "JPEG", quality=90)
-    #     shutil.copyfile("/ibex/user/kellyt/windowz/winsyn_riyal/labels_8bit/" + line + ".png", f"{outdir}/labels/{line}.png")
+    for line in slines:
+        print(line)
+        line = line.strip()
+
+        op = f"{outdir}/rgb/{line}.jpg"
+
+        if os.path.exists(op):
+            continue
+
+        img = Image.open(f"/ibex/user/kellyt/windowz/winsyn_riyal/rgb/{line}.png")
+        img = img.convert("RGB")
+
+        # background = Image.new('RGBA', img.size, (255, 255, 255))
+        # alpha_composite = Image.alpha_composite(background, img)
+
+        img.save(op, "JPEG", quality=90)
+        shutil.copyfile("/ibex/user/kellyt/windowz/winsyn_riyal/labels_8bit/" + line + ".png", f"{outdir}/labels/{line}.png")
 
 # fixed real, some synthetic
-for i in range(12, 15):
-
-    for z in range (5) if i <= 16 else [1]:
-        syn = 2 ** i
-
-        print(f">>>> {syn} {i}")
-
-        with open(f"{outdir}/real_{syn}_{z}.txt", "w") as f:
-
-            random.shuffle(rlines)
-            random.shuffle(slines)
-
-            lines = [*rlines]
-            lines.extend(slines[:syn])
-
-            for line in lines:
-                line = line.strip()
-                f.write(f"{line}\n")
+# for i in range(12, 15):
+#
+#     for z in range (5) if i <= 16 else [1]:
+#         syn = 2 ** i
+#
+#         print(f">>>> {syn} {i}")
+#
+#         with open(f"{outdir}/real_{syn}_{z}.txt", "w") as f:
+#
+#             random.shuffle(rlines)
+#             random.shuffle(slines)
+#
+#             lines = [*rlines]
+#             lines.extend(slines[:syn])
+#
+#             for line in lines:
+#                 line = line.strip()
+#                 f.write(f"{line}\n")
 
 # fixed synthetic, some real
 # for i in range(0, 12):
