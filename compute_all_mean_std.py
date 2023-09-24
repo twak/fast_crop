e9e9fdee79df8721d78a1510742a44f224bba012
twak <twakered@gmail.com>
2023-09-24 13:50:55 +0100

    threaded + _c

--- compute_all_mean_std.py
+++ compute_all_mean_std.py
@@ -8,6 +8,10 @@ from PIL import Image
 
 from pathlib import Path
 
+from pathlib import Path
+import contextlib
+import concurrent.futures
+
 def write_mean(dir, count=2048):
 
 
@@ -65,11 +69,49 @@ def write_mean(dir, count=2048):
 
 if __name__ == "__main__":
 
-    os.makedirs("means", exist_ok=True)
+    _pool = concurrent.futures.ThreadPoolExecutor()
 
-    for dir, _ in  [["1024ms","png"], ["256ms","png"], ["attribs","txt"], ["edges","png"], ["normals","png"],
-       ["rgb_albedo","png"], ["texture_rot","png"], ["128ms","png"], ["512ms","png"], ["col_per_obj","png"],
-       ["labels","png"], ["phong_diffuse","png"], ["voronoi_chaos","png"], ["2048ms","png"], ["64ms","png"],
-       ["diffuse","png"], ["rgb","png"], ["rgb_histomatched","png"],
-       ["rgb_exposed","png"], ["rgb_exposed_histomatched","png"] ]:
-        write_mean(dir)
+    os.makedirs("means", exist_ok=True)
+    for dir in [
+        "0cen" ,
+        "128spp" ,
+        "12cen" ,
+        "16spp" ,
+        "1spp" ,
+        "256spp" ,
+        "2cen" ,
+        "2spp" ,
+        "32spp" ,
+        "4cen" ,
+        "4spp" ,
+        "512spp" ,
+        "64spp" ,
+        "8cen" ,
+        "8spp" ,
+        "dayonly" ,
+        "dayonly_exposed" ,
+        "fixedsun" ,
+        "fixedsun_exposed" ,
+        "monomat" ,
+        "nightonly" ,
+        "nightonly_exposed" ,
+        "nobounce" ,
+        "nobounce_exposed" ,
+        "nosun" ,
+        "nosun_exposed" ,
+        "notransmission" ,
+        "rgb" ,
+        "rgb_albedo" ,
+        "rgb_depth" ,
+        "rgb_exposed"]:
+        _pool.submit(write_mean, dir)
+
+print ("all submitted!")
+
+    # _c
+    # for dir, _ in  [["1024ms","png"], ["256ms","png"], ["attribs","txt"], ["edges","png"], ["normals","png"],
+    #    ["rgb_albedo","png"], ["texture_rot","png"], ["128ms","png"], ["512ms","png"], ["col_per_obj","png"],
+    #    ["labels","png"], ["phong_diffuse","png"], ["voronoi_chaos","png"], ["2048ms","png"], ["64ms","png"],
+    #    ["diffuse","png"], ["rgb","png"], ["rgb_histomatched","png"],
+    #    ["rgb_exposed","png"], ["rgb_exposed_histomatched","png"] ]:
+    #     write_mean(dir)
