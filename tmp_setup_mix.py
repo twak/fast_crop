846a02555dd210bb7a76ab621ef0a59f1636abac
twak <twakered@gmail.com>
2024-01-12 19:49:52 +0000

    easy4 mix for fig 7

--- tmp_setup_mix.py
+++ tmp_setup_mix.py
@@ -10,40 +10,40 @@ create a new dataset that mixes synthetic and real. Create a bunch of split file
 '''
 
 os.makedirs(f"{outdir}/rgb", exist_ok=True)
-os.makedirs(f"{outdir}/labels", exist_ok=True)
+os.makedirs(f"{outdir}/labels_easy4", exist_ok=True)
 
 rlines, slines = [], []
 
-with open("/ibex/user/kellyt/winlab_5/4096.txt", "r") as f:
+with open("/ibex/user/kellyt/winlab_5/easy_train.txt", "r") as f:
 # with open("/ibex/user/kellyt/winlab_5/2048.txt", "r") as f:
     rlines = f.readlines()
-    # for line in rlines:
-    #     print(line)
-    #     line = line.strip()
-    #     shutil.copyfile("/ibex/user/kellyt/winlab_5/rgb/" + line + ".jpg", f"{outdir}/rgb/{line}.jpg")
-    #     shutil.copyfile("/ibex/user/kellyt/winlab_5/labels/" + line + ".png", f"{outdir}/labels/{line}.png")
+    for line in rlines:
+        print(line)
+        line = line.strip()
+        shutil.copyfile("/ibex/user/kellyt/winlab_5/rgb/" + line + ".jpg", f"{outdir}/rgb/{line}.jpg")
+        shutil.copyfile("/ibex/user/kellyt/winlab_5/labels_easy4/" + line + ".png", f"{outdir}/labels_easy4/{line}.png")
 
 
 # with open("/ibex/user/kellyt/windowz/winsyn_riyal/16384.txt", "r") as f:
 with open("/ibex/user/kellyt/windowz/winsyn_riyal/2048.txt", "r") as f:
     slines = f.readlines()
-    # for line in slines:
-    #     print(line)
-    #     line = line.strip()
-    #
-    #     op = f"{outdir}/rgb/{line}.jpg"
-    #
-    #     if os.path.exists(op):
-    #         continue
-    #
-    #     img = Image.open(f"/ibex/user/kellyt/windowz/winsyn_riyal/rgb/{line}.png")
-    #     img = img.convert("RGB")
-    #
-    #     # background = Image.new('RGBA', img.size, (255, 255, 255))
-    #     # alpha_composite = Image.alpha_composite(background, img)
-    #
-    #     img.save(op, "JPEG", quality=90)
-    #     shutil.copyfile("/ibex/user/kellyt/windowz/winsyn_riyal/labels_8bit/" + line + ".png", f"{outdir}/labels/{line}.png")
+    for line in slines:
+        print(line)
+        line = line.strip()
+
+        op = f"{outdir}/rgb/{line}.jpg"
+
+        if os.path.exists(op):
+            continue
+
+        img = Image.open(f"/ibex/user/kellyt/windowz/winsyn_riyal/rgb/{line}.png")
+        img = img.convert("RGB")
+
+        # background = Image.new('RGBA', img.size, (255, 255, 255))
+        # alpha_composite = Image.alpha_composite(background, img)
+
+        img.save(op, "JPEG", quality=90)
+        shutil.copyfile("/ibex/user/kellyt/windowz/winsyn_riyal/labels_easy4/" + line + ".png", f"{outdir}/labels_easy4/{line}.png")
 
 # fixed real, some synthetic
 # for i in range(12, 15):
@@ -67,22 +67,22 @@ with open("/ibex/user/kellyt/windowz/winsyn_riyal/2048.txt", "r") as f:
 #                 f.write(f"{line}\n")
 
 # fixed synthetic, some real
-for i in [12]: # range(0, 12):
+for i in range(1, 12):
 
-    real = 2 ** i
-    for z in [0]: # range (5):
+    real = 2 ** i  # 2...2048 inclusive
+    print(f">>>> {real} {i}")
 
-        print(f">>>> {real} {i}")
+    with open(f"{outdir}/easy_mix_{real}.txt", "w") as f:
 
-        with open(f"{outdir}/mix_{real}_{z}.txt", "w") as f:
+        # random.shuffle(rlines)
+        # random.shuffle(slines)
 
-            random.shuffle(rlines)
-            random.shuffle(slines)
+        lines = [*slines]
+        lines.extend(rlines[:real])
 
-            lines = [*slines]
-            lines.extend(rlines[:real])
+        random.shuffle(lines)
 
-            for line in lines:
-                line = line.strip()
-                f.write(f"{line}\n")
+        for line in lines:
+            line = line.strip()
+            f.write(f"{line}\n")
 
